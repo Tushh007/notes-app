c2f0300762aa06191df1f92e7b258d2c3c61ad6f
Tushar Langer <tushar.langer@joulon.com>
2020-09-26 21:20:03 +0530

    first commit

---
+++ apps.js
@@ -0,0 +1,63 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// customize yargs verison
+yargs.version('1.1.0')
+
+// create add command
+yargs.command ({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// create a remove command
+yargs.command ({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// create a list command
+yargs.command ({
+    command: 'list',
+    describe: 'List notes',
+    handler: function () {
+        console.log('listing all notes!')
+    }
+})
+
+// Create a Read command
+yargs.command ({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note!')
+    }
+})
+
+yargs.parse()
